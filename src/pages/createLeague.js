3a88179285ad77bec96d06d532f624e9c80bd5c2
Le Doan Tan Tri <ledoantantri@gmail.com>
2023-12-12 09:16:52 +0700

    add toastify and handle input fields

--- src/pages/createLeague.js
+++ src/pages/createLeague.js
@@ -7,7 +7,12 @@ import Layout from '~/components/layout';
 import Input from '~/components/input';
 import InputShort from '~/components/input-short';
 import ImageButton from '~/components/imageButton';
-import { Switch } from '@mui/material';
+import { FormControlLabel, FormGroup, Switch } from '@mui/material';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import { toast } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const CreateLeague = () => {
   const navigate = useNavigate();
@@ -20,6 +25,57 @@ const CreateLeague = () => {
   const [competitionFormat, setCompetitionFormat] = useState('');
   const [numOfTeams, setNumOfTeams] = useState('');
   const [privacy, setPrivacy] = useState('');
+
+  const [enrollmentDeadline, setEnrollmentDeadline] = useState('');
+  const [numOfPeople, setNumOfPeople] = useState('');
+
+  const ShowPublicEnrollment = () => {
+    if (privacy === 'Public') {
+      return (
+        <div className={styles.createLeagueForm}>
+          <InputShort
+            label="Enrollment Deadline"
+            placeholder="Enrollment deadline"
+            value={enrollmentDeadline}
+            onChange={(e) => setEnrollmentDeadline(e.target.value)}
+          />
+
+          <Input
+            label="Number of People"
+            placeholder="Number of People"
+            value={numOfPeople}
+            onChange={(e) => setNumOfPeople(e.target.value)}
+          />
+        </div>
+      );
+    }
+    return null;
+  };
+
+  const handleCreateLeague = () => {
+    if (
+      leagueName.length < 1 ||
+      phoneNumber.length < 1 ||
+      location.length < 1 ||
+      ageRange.length < 1 ||
+      numOfPlayers.length < 1 ||
+      competitionFormat.length < 1 ||
+      numOfTeams.length < 1 ||
+      privacy.length < 1
+    ) {
+      alert('Please fill out all fields');
+    } else {
+      toast.success('League created successfully!');
+      setLeagueName('');
+      setPhoneNumber('');
+      setLocation('');
+      setAgeRange('');
+      setNumOfPlayers('');
+      setCompetitionFormat('');
+      setNumOfTeams('');
+      setPrivacy('');
+    }
+  };
 
   return (
     <Layout>
@@ -94,16 +150,38 @@ const CreateLeague = () => {
       </div>
 
       <div className={styles.createLeagueForm}>
-        <InputShort
-          label="Privacy"
-          placeholder="Privacy"
-          value={privacy}
-          styles={styles.alignLeft}
-          onChange={(e) => setPrivacy(e.target.value)}
-        />
+        <div className={styles.form3}>
+          <InputShort
+            label="Privacy"
+            placeholder="Privacy"
+            value={privacy}
+            styles={styles.alignLeft}
+            onChange={(e) => setPrivacy(e.target.value)}
+          />
+        </div>
 
-        <Switch Allow public enrollment />
+        <div className={styles.form4}>
+          <FormGroup>
+            <FormControlLabel
+              control={
+                <Switch
+                  disabled={privacy === 'Private'}
+                  checked={privacy === 'Public'}
+                />
+              }
+              label="Allow public enrollment"
+            />
+          </FormGroup>
+        </div>
       </div>
+
+      <ShowPublicEnrollment />
+
+      <div className={styles.createLeagueForm}>
+        <Button text="Create League" onClick={handleCreateLeague}></Button>
+      </div>
+
+      <ToastContainer />
     </Layout>
   );
 };
