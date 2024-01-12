4e8c295c4d1b5a9f9d0fdec32238d62ddf894c0a
Tran Gia Bao <tranb6778@gmail.com>
2024-01-12 18:24:52 +0700

    Add match detail edit

---
+++ src/components/leagueInfoTabs/closed/(test) samplePlayers2.js
@@ -0,0 +1,40 @@
+const players2 = [
+  {
+    id: 1,
+    name: 'Player 1',
+    jerseyNumber: 10,
+    team: 'Team A',
+    goals: 11,
+    yellow: 2,
+    red: 1,
+  },
+  {
+    id: 2,
+    name: 'Player 2',
+    jerseyNumber: 7,
+    team: 'Team B',
+    goals: 3,
+    yellow: 4,
+    red: 0,
+  },
+  {
+    id: 3,
+    name: 'Player 3',
+    jerseyNumber: 9,
+    team: 'Team C',
+    goals: 5,
+    yellow: 1,
+    red: 0,
+  },
+  {
+    id: 4,
+    name: 'Player 4',
+    jerseyNumber: 11,
+    team: 'Team A',
+    goals: 11,
+    yellow: 1,
+    red: 0,
+  },
+];
+
+export default players2;
